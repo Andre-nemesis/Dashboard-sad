bf85c9a108dfc27a8bbdf193c3e0edeab0efb944
andre <andrecasimio@gmail.com>
2023-05-13 19:06:08 -0300

    third comit

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-# pip install streamlit pandas matplotlib seaborn
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Fonte de Dados
-# https://www.kaggle.com/datasets/whenamancodes/student-performance
-
-# Especificando o título da página e o ícone
-st.set_page_config(page_title="Dashboard - Student Dataset", page_icon=":books:")
-
-# sidebar
-st.sidebar.title("Configurações de Exibição")
-
+#seleção de gráficos
 gsheets_show_id = st.sidebar.radio("Selecione o Dataset", ("Matemática", "Português"))
-
-st.sidebar.subheader("Selecione o que deseja exibir")
-show_dataset = st.sidebar.checkbox("Dados do Dataset")
-show_dataset_description = st.sidebar.checkbox("Descrição do Dataset")
-
 graph1_type = st.sidebar.selectbox("Gráfico 1: Selecione o tipo de gráfico", ("Barra", "Pizza", "Dispersão", "Histograma", "Boxplot"))
 
-# Carregando o dataset
 gsheets_math_id = "1392993996"
 gsheets_portuguese_id = "0"
 
@@ -35,10 +20,11 @@ def load_data(sheets_url):
 
 data = load_data(gsheets_url)
 
-# Adicionando um título
-st.title("Análise de Dados do Dataset de Estudantes")
+#opções de viszualização do dataset
+st.sidebar.subheader("Selecione o que deseja exibir")
+show_dataset = st.sidebar.checkbox("Dados do Dataset")
+show_dataset_description = st.sidebar.checkbox("Descrição do Dataset")
 
-# Descritivo do dataset
 if show_dataset_description:
     st.subheader("Descrição do Dataset")
 
@@ -81,35 +67,159 @@ if show_dataset:
     st.subheader("Conjunto de Dados")
     st.dataframe(data)
 
+#Questão 1
+#grafico de barras
+st.subheader("Distribuição de Idade dos alunos na Escola GP")
+age = data.loc[0:350,'age'].value_counts()
+fig_age, ax_age = plt.subplots()
+sns.barplot(x=age.index, y=age.values)
+ax_age.set_xlabel('Idade')
+ax_age.set_ylabel('Número de Estudantes da Escola GP')
+#aparececr números emcima das barras
+for i in ax_age.containers:
+    ax_age.bar_label(i,)
+st.pyplot(fig_age)
 
-# Gráficos e tabelas
-
-# Adicionando um gráfico de barras para mostrar a distribuição de gênero dos estudantes
-st.subheader("Distribuição de Gênero dos Estudantes")
-gender_count = data['sex'].value_counts()
-fig, ax = plt.subplots()
-if graph1_type == "Pizza":
-    ax.pie(gender_count.values, labels=gender_count.index, autopct='%1.1f%%')
-    ax.set_title('Distribuição de Gênero dos Estudantes')
-else:
-    sns.barplot(x=gender_count.index, y=gender_count.values)
-    ax.set_xlabel('Gênero')
-    ax.set_ylabel('Número de Estudantes')
-st.pyplot(fig)
-
-# Adicionando uma tabela para mostrar a média de idade dos estudantes por escola
-st.subheader("Média de Idade dos Estudantes por Escola")
-school_mean_age = data.groupby('school')['age'].mean()
+#legenda
+st.subheader("Média de idade dos alunos na Escola GP")
+agge = data.loc[0:348,'age'].mean()
+school_mean_age = {'Media GP':agge}
 st.table(school_mean_age)
 
-# Adicionando um gráfico de dispersão para mostrar a relação entre o tempo de estudo semanal e o número de faltas
-st.subheader("Relação entre Tempo de Estudo Semanal e Número de Faltas")
-fig, ax = plt.subplots()
-sns.scatterplot(x=data['studytime'], y=data['absences'])
-ax.set_xlabel('Tempo de Estudo Semanal')
-ax.set_ylabel('Número de Faltas')
-st.pyplot(fig)
+#Questão 2
+#grafico de pizza
+st.subheader("Endereços dos alunos da escola MS")
+address_count = data.loc[349:398,'address'].value_counts()
+fig_address, ax_address = plt.subplots()
+ax_address.pie(address_count.values, labels=address_count.index, autopct='%1.1f%%')
+ax_address.set_title('Distribuição por zona Urbana e Rural')
+st.pyplot(fig_address)
+#legenda
+st.subheader("Moda do endereço dos alunos na escola MS")
+school_mode_address = data.loc[349:398,'address'].mode()
+st.table(school_mode_address)
 
-# Adicionando uma seção para mostrar as estatísticas descritivas dos atributos numéricos do dataset
-st.subheader("Estatísticas Descritivas dos Atributos Numéricos")
-st.write(data.describe())
+#Questão 3
+#grafico de linnha
+st.subheader("Tempo de viagem dos alunos da escola GP")
+traveltime = data.loc[0:348,'traveltime'].value_counts()
+fig_traveltime, ax_traveltime = plt.subplots()
+sns.lineplot(x=traveltime.index, y=traveltime.values)
+ax_traveltime.set_xlabel('Quantidade de horas')
+ax_traveltime.set_ylabel('Quantidade de alunos')
+st.pyplot(fig_traveltime)
+
+#legendas
+st.subheader('Mediana do tempo de viagem dos alunos que estudam na escola GP')
+traveltime_date = data.loc[0:348,'traveltime'].mean()
+school_median_traveltime = {'GP':traveltime_date}
+st.table(school_median_traveltime)
+
+#Questão 4
+#grafico de barras
+st.subheader('Idade dos alnos que possuem apoio educacional extra na escola MS')
+schoolsup = data.loc[349:398,['schoolsup','age']].value_counts()
+fig_schoolsup,ax_schoolsup = plt.subplots()
+sns.barplot(data=data,x='school',y='age',hue='schoolsup')
+ax_schoolsup.set_xlabel('Apoio Educacional')
+ax_schoolsup.set_ylabel('Idade')
+for i in ax_schoolsup.containers:
+    ax_schoolsup.bar_label(i,)
+st.pyplot(fig_schoolsup)
+
+#legenda
+st.subheader('desvio padrão da idade dos alunos que têm apoio educacional extra na escola MS')
+df_school_sup = pd.DataFrame(schoolsup)
+st.table(df_school_sup.std())
+
+#Questão 5
+#grafico de barras
+school_median_studytime_and_Pstatus = data.loc[0:348,['Pstatus','studytime']].value_counts()
+studytime = {'GP':school_median_studytime_and_Pstatus['A']}
+df_studytime = pd.DataFrame(studytime)
+fig_studytime, ax_studytime = plt.subplots()
+sns.barplot(data=data,x='Pstatus',y='studytime',hue='school')
+ax_studytime.set_xlabel('Tipo do Relacionamento Familiar')
+ax_studytime.set_ylabel('Tempo de Estudo')
+for i in ax_studytime.containers:
+    ax_studytime.bar_label(i,)
+st.pyplot(fig_studytime)
+#legenda
+st.subheader('Média do tempo semanal de estudo dos alunos cujos pais estão separados na escola GP')
+st.table(df_studytime.mean())
+
+#Questão 6
+#grafico de pizza
+reason_count = data.loc[349:398,'reason'].value_counts()
+fig_reason, ax_reason = plt.subplots()
+ax_reason.pie(reason_count.values, labels=reason_count.index, autopct='%1.1f%%')
+ax_reason.set_title('Motivo da escolha da escola MS')
+st.pyplot(fig_reason)
+
+#legenda
+st.subheader('Moda do motivo pelo qual os alunos escolheram a escola MS')
+school_moda_reason = data.loc[349:398,'reason'].mode()
+st.table(school_moda_reason)
+
+#Questão 7
+#grafico de linhas
+st.subheader("Números de faltas")
+absences = data.loc[0:348,'absences'].value_counts()
+fig_absences, ax_absences = plt.subplots()
+sns.lineplot(x=absences.index, y=absences.values)
+ax_absences.set_xlabel('Números de faltas')
+ax_absences.set_ylabel('Quantidade de faltas')
+st.pyplot(fig_absences)
+
+#legenda
+st.subheader('Mediana do número de faltas dos alunos que frequentam a escola GP')
+absences = data.loc[0:348,'absences'].median()
+school_median_absences= {'absences - GP':absences}
+st.table(school_median_absences)
+
+#Questão 8
+#grafico barras
+st.subheader("Saúde dos alunos que frequentam atividades extracurriculares")
+school_health_activities = data.loc[349:398,['health','activities']].value_counts()
+
+fig_health, ax_health = plt.subplots()
+sns.barplot(data=data,x='activities',y='health',hue='school')
+ax_health.set_xlabel('Realiza Atividade Extracurricular')
+ax_health.set_ylabel('Qualidade da Sáude')
+#aparecer números emcima das barras
+for i in ax_health.containers:
+    ax_health.bar_label(i,)
+st.pyplot(fig_health)
+
+#legenda
+st.subheader('Desvio padrão do nível de saúde dos alunos que frequentam atividades extracurriculares na escola MS')
+school_health_std = school_health_activities
+st.table(school_health_std)
+
+#Questão 9
+#grafico de barras
+st.subheader('Quantidade de alunos que realizaram atiidades extracurriculares da escola GP')
+activities = data.loc[0:348,'activities'].value_counts()
+fig_activities,ax_activities = plt.subplots()
+sns.barplot(x=activities.index,y=activities.values)
+ax_activities.set_ylabel('número de alunos')
+ax_activities.set_xlabel('Respostas')
+#aparececr números emcima das barras
+for i in ax_activities.containers:
+    ax_activities.bar_label(i,)
+st.pyplot(fig_activities)
+
+#Questão 10
+#grafico de linha
+st.subheader("Números de consumo de Alcool pelos estudantes")
+dalc = data.loc[0:348,'Dalc'].value_counts()
+fig_dalc, ax_dalc = plt.subplots()
+sns.lineplot(x=dalc.index, y=dalc.values)
+ax_dalc.set_xlabel('Quantidade de consumo na semana')
+ax_dalc.set_ylabel('Números de Pessoas')
+st.pyplot(fig_dalc)
+#legenda
+st.subheader('Moda do consumo de álcool dos alunos da escola MS durante a semana de trabalho')
+dalc = data.loc[349:398,'Dalc'].mode()
+school_moda_dalc = {'Consume - MS':dalc}
+st.table(school_moda_dalc)
